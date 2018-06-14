959c41be7981e600f1f383d104dbf721c1299d19
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-06-14 21:28:36 +0300

    thread list and show

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,21 @@
 <template>
-
+  <div>
+    <h1>Welcome to the forum</h1>
+    <ThreadList :threads="Object.values(threads)" />
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld'
-}
+  import sourceData from '@/data'
+  import ThreadList from './ThreadList'
+  export default {
+    components: { ThreadList },
+    data () {
+      return {
+        threads: Object.values(sourceData.threads),
+        posts: sourceData.posts,
+        users: sourceData.users
+      }
+    }
+  }
 </script>
