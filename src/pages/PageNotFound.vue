ec3a6c53e88de31ca22104d6049eae238c58053f
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-06-15 18:16:47 +0300

    page not found

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="col-full">
+        <h1>
+            Not Found
+        </h1>
+        <p>Ooops, we couldn't find what are looking for. What don't you
+            <router-link :to="{name:'Home'}">go home instead?</router-link>
+        </p>
+    </div>
+</template>
+ 
+
+<style scoped>
+h1 {
+  font-size: 100px;
+}
+p {
+  font-size: 50px;
+  font-weight: 100;
+}
+div {
+  text-align: center;
+}
+</style>
