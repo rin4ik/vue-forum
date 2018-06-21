716195ad86396115ab45187622473f173412f4c0
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-06-21 19:51:23 +0300

    let the discussion begin

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,8 +1,18 @@
 <template>
-    <div class="col-large push-top">
-        <h1>{{thread.title}}</h1>
-        <PostList :posts="posts" />
-    </div>
+  <div class="col-large push-top">
+    <h1>{{thread.title}}</h1>
+    <PostList :posts="posts" />
+    <form @submit.prevent="addPost">
+      <div class="for-group">
+        <textarea class="form-input" v-model="newPostText"></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">
+          Submit post
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -21,7 +31,24 @@
     },
     data () {
       return {
-        thread: sourceData.threads[this.id]
+        thread: sourceData.threads[this.id],
+        newPostText: ''
+      }
+    },
+    methods: {
+      addPost () {
+        const postId = 'greatPost' + Math.random()
+        const post = {
+          text: this.newPostText,
+          publishetAt: Math.floor(Date.now() / 1000),
+          threadId: this.id,
+          userId: 'u4r8XCziZEWEXsj2UIKNHBoDh0n2',
+          '.key': postId
+        }
+        this.$set(sourceData.posts, postId, post)
+        this.$set(this.thread.posts, postId, postId)
+        this.$set(sourceData.users[post.userId].posts, postId, postId)
+        this.newPostText = ''
       }
     },
     computed: {
