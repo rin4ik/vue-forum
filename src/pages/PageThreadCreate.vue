83127bea07c8de68013a1ff2023b3ac6435dd2bb
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-06-23 20:08:06 +0300

    threadEditor

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -3,25 +3,11 @@
 
           <h1>Create new thread in <i>{{forum.name}}</i></h1>
 
-          <form @submit.prevent="save">
-              <div class="form-group">
-                <label for="thread_title">Title:</label>
-                <input v-model="title" type="text" id="thread_title" class="form-input" name="title">
-              </div>
-
-              <div class="form-group">
-                <label for="thread_content">Content:</label>
-                <textarea v-model="content" id="thread_content" class="form-input" name="content" rows="8" cols="140"></textarea>
-              </div>
-
-              <div class="btn-group">
-                <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
-                <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
-              </div>
-          </form>
+         <ThreadEditor @save="save" @cancel="cancel" />
       </div>
 </template>
 <script>
+    import ThreadEditor from '@/components/ThreadEditor'
     export default {
       props: {
         forumId: {
@@ -29,18 +15,15 @@
           required: true
         }
       },
-      data () {
-        return {
-          title: '',
-          content: ''
-        }
+      components: {
+        ThreadEditor
       },
       methods: {
-        save () {
+        save ({title, content}) {
           this.$store.dispatch('createThread', {
             forumId: this.forum['.key'],
-            title: this.title,
-            text: this.content
+            title,
+            content
           }).then(thread => {
             this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
           })
