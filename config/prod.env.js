9b4da3c9a386b4c4b6e3c6e89b6a349d6c55935b
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-06-29 19:57:46 +0300

    using .env file

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,11 @@
 'use strict'
+require('dotenv').config()
 module.exports = {
   FIREBASE_NODE_ENV: '"production"',
-  FIREBASE_API_KEY: "'AIzaSyDdvDilPc5DXx10pPBOF4_vykMHFjoL-Nk'",
-  FIREBASE_AUTH_DOMAIN: "'vue-schoolforum.firebaseapp.com'",
-  FIREBASE_DATABASE_URL: "'https://vue-schoolforum.firebaseio.com'",
-  FIREBASE_PROJECT_ID: "'vue-schoolforum'",
-  FIREBASE_STORAGE_BUCKET: "'vue-schoolforum.appspot.com'",
-  FIREBASE_MESSAGING_ID: "'85756752568'"
+  FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+  FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+  FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+  FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+  FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+  FIREBASE_MESSAGING_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_ID)
 }
